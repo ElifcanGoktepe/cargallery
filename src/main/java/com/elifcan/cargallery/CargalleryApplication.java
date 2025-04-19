77ba691aaf4e41aa762ac24c7a71d040ce91cb7b
elifcanstudent <elifcanmeryem.goktepe@bilgeadamboost.com>
2025-04-19 10:35:02 +0200

    Initial commit

---
+++ src/main/java/com/elifcan/cargallery/CargalleryApplication.java
@@ -0,0 +1,13 @@
+package com.elifcan.cargallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elifcan/cargallery/CargalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elifcan.cargallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
