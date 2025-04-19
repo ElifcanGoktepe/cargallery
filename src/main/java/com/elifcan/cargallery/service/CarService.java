2718f9b99dc271f7997f5793f1bae8c0074cdf86
sercan94 <sercan.imir1994@gmail.com>
2025-04-19 12:51:17 +0300

    car ve user service kısımları eklendi.

---
+++ src/main/java/com/elifcan/cargallery/service/CarService.java
@@ -0,0 +1,14 @@
+package com.elifcan.cargallery.service;
+
+
+import com.elifcan.cargallery.repository.CarRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CarService {
+    private final CarRepository repository;
+
+
+}
