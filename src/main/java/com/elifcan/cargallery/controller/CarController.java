9024675cf14d701806662d371efeb4c4131f54c4
elifcanstudent <elifcanmeryem.goktepe@bilgeadamboost.com>
2025-04-19 13:35:39 +0200

    RestApi ve Car Controller da düzenleme yapıldı

--- src/main/java/com/elifcan/cargallery/controller/CarController.java
+++ src/main/java/com/elifcan/cargallery/controller/CarController.java
@@ -1,9 +1,12 @@
 package com.elifcan.cargallery.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.elifcan.cargallery.config.RestApi.CAR;
 
 @RestController
 @RequiredArgsConstructor
