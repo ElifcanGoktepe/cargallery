5765356a4a246a94809c1f87bd4436d04848e4ca
mberkanakblt <berkans.akbuluts@gmail.com>
2025-04-20 09:15:33 +0300

    UserController düzenlendi

--- src/main/java/com/elifcan/cargallery/exception/CarGalleryException.java
+++ src/main/java/com/elifcan/cargallery/exception/CarGalleryException.java
@@ -1,5 +1,8 @@
 package com.elifcan.cargallery.exception;
 
+import lombok.Getter;
+
+@Getter
 public class CarGalleryException extends RuntimeException {
     private ErrorType errorType;
     public CarGalleryException (ErrorType errorType){
